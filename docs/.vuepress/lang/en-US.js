4aa4e637524379f27cce123b306d1cb25ae2625d
InkoHX <inkohx@gmail.com>
2019-05-04 23:39:28 +0900

    Add guide link

--- docs/.vuepress/lang/en-US.js
+++ docs/.vuepress/lang/en-US.js
@@ -1,16 +1,20 @@
 module.exports.getNavbar = function() {
   return [
     {
+      text: 'Guide',
+      link: '/guide/'
+    },
+    {
       text: 'Discord',
       link: 'https://discord.gg/JJhtTvG'
     },
     {
-      text: 'DBL',
-      link: 'https://discordbots.org/bot/531097309748920371'
-    },
-    {
       text: 'Invite bot',
       link: 'https://discordapp.com/oauth2/authorize?client_id=531097309748920371&permissions=60422&scope=bot'
+    },
+    {
+      text: 'DBL',
+      link: 'https://discordbots.org/bot/531097309748920371'
     }
   ]
 }
--- docs/.vuepress/lang/ja-JP.js
+++ docs/.vuepress/lang/ja-JP.js
@@ -1,16 +1,20 @@
 module.exports.getNavbar = function() {
   return [
     {
+      text: 'ガイド',
+      link: '/guide/'
+    },
+    {
       text: 'Discord',
       link: 'https://discord.gg/JJhtTvG'
     },
     {
-      text: 'DBL',
-      link: 'https://discordbots.org/bot/531097309748920371'
-    },
-    {
       text: 'BOTを招待',
       link: 'https://discordapp.com/oauth2/authorize?client_id=531097309748920371&permissions=60422&scope=bot'
+    },
+    {
+      text: 'DBL',
+      link: 'https://discordbots.org/bot/531097309748920371'
     }
   ]
 }
